b43b96d57ba5351d4aa1504ffc83a44fd3a98451
USER <USER@USER-PC>
2018-07-26 10:36:09 +0530

    cimmit my first jenkins_code

---
+++ src/test/java/Testcases/TC_01.java
@@ -0,0 +1,21 @@
+package Testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TC_01 {
+	
+	@Test
+	
+	public static void TC_01_signin()
+	
+	{
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://freshdesignweb.com/demo/html5-registration-form/");
+		
+		
+		
+	}
+
+}
